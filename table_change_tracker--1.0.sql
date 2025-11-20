5b0186f309325970f2910d558a493517587b99d9
User <ulyanovskiy.2001@mail.ru>
2025-11-20 12:51:36 +0300

    feat: add basic change counter logic

--- table_change_tracker--1.0.sql
+++ table_change_tracker--1.0.sql
@@ -1,6 +1,6 @@
 \echo Use "CREATE EXTENSION table_change_tracker" to load this file. \quit
 
-CREATE OR REPLACE FUNCTION hello_world()
+CREATE OR REPLACE FUNCTION get_change_counter()
 RETURNS text
-AS 'MODULE_PATHNAME', 'hello_world'
+AS 'MODULE_PATHNAME', 'get_change_counter'
 LANGUAGE C STRICT;
--- table_change_tracker.c
+++ table_change_tracker.c
@@ -1,12 +1,70 @@
 #include "postgres.h"
 #include "fmgr.h"
 #include "utils/builtins.h"
+#include "tcop/utility.h"
+#include "executor/executor.h"
 
 PG_MODULE_MAGIC;
 
-PG_FUNCTION_INFO_V1(hello_world);
+static ProcessUtility_hook_type prev_ProcessUtility = NULL;
+static ExecutorStart_hook_type prev_ExecutorStart = NULL;
 
-Datum hello_world(PG_FUNCTION_ARGS)
+static uint64 change_counter = 0;
+
+Datum get_change_counter(PG_FUNCTION_ARGS);
+
+static void track_utility(
+    PlannedStmt *pstmt, const char *queryString, bool readOnlyTree, ProcessUtilityContext context,
+    ParamListInfo params, QueryEnvironment *queryEnv, DestReceiver *dest, QueryCompletion *qc);
+
+static void track_executor_start(QueryDesc *queryDesc, int eflags);
+
+void _PG_init(void);
+void _PG_fini(void);
+
+PG_FUNCTION_INFO_V1(get_change_counter);
+
+Datum get_change_counter(PG_FUNCTION_ARGS)
 {
-    PG_RETURN_TEXT_P(cstring_to_text("Hello world!"));
+    PG_RETURN_INT64(change_counter);
+}
+
+static void track_utility(
+    PlannedStmt *pstmt, const char *queryString, bool readOnlyTree, ProcessUtilityContext context,
+    ParamListInfo params, QueryEnvironment *queryEnv, DestReceiver *dest, QueryCompletion *qc)
+{
+    if (prev_ProcessUtility)
+        prev_ProcessUtility(pstmt, queryString, readOnlyTree, context, params, queryEnv, dest, qc);
+    else
+        standard_ProcessUtility(pstmt, queryString, readOnlyTree, context, params, queryEnv, dest, qc);
+}
+
+static void track_executor_start(QueryDesc *queryDesc, int eflags)
+{
+    CmdType operation = queryDesc->operation;
+
+    if (operation == CMD_INSERT || operation == CMD_UPDATE || operation == CMD_DELETE)
+    {
+        change_counter++;
+    }
+
+    if (prev_ExecutorStart)
+        prev_ExecutorStart(queryDesc, eflags);
+    else
+        standard_ExecutorStart(queryDesc, eflags);
+}
+
+void _PG_init(void)
+{
+    prev_ExecutorStart = ExecutorStart_hook;
+    ExecutorStart_hook = track_executor_start;
+
+    prev_ProcessUtility = ProcessUtility_hook;
+    ProcessUtility_hook = track_utility;
+}
+
+void _PG_fini(void)
+{
+    ExecutorStart_hook = prev_ExecutorStart;
+    ProcessUtility_hook = prev_ProcessUtility;
 }
