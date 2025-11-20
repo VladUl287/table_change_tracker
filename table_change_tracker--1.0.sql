57e2ba7b2b2806385ac030e08b8a7cce9b3b9327
User <ulyanovskiy.2001@mail.ru>
2025-11-20 20:20:04 +0300

    fix: change return type for get_change_counter function

--- table_change_tracker--1.0.sql
+++ table_change_tracker--1.0.sql
@@ -1,7 +1,7 @@
 \echo Use "CREATE EXTENSION table_change_tracker" to load this file. \quit
 
 CREATE OR REPLACE FUNCTION get_change_counter()
-RETURNS text
+RETURNS bigint
 AS 'MODULE_PATHNAME', 'get_change_counter'
 LANGUAGE C STRICT;
 
