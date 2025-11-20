c4ea9edbdcb8963fc615c30d33e3e84ba8120a1c
User <ulyanovskiy.2001@mail.ru>
2025-11-20 20:28:28 +0300

    refactor: remove sql proxy for hello_world function

--- table_change_tracker--1.0.sql
+++ table_change_tracker--1.0.sql
@@ -4,8 +4,3 @@ CREATE OR REPLACE FUNCTION get_change_counter()
 RETURNS bigint
 AS 'MODULE_PATHNAME', 'get_change_counter'
 LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION hello_world()
-RETURNS text
-AS 'MODULE_PATHNAME', 'hello_world'
-LANGUAGE C STRICT;
