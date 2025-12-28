ab58e3bbb939d21d3a924f89c973c4e0b726fdc7
User <ulyanovskiy.2001@mail.ru>
2025-12-28 17:47:00 +0300

    refactor: tracker logic

--- table_change_tracker.c
+++ table_change_tracker.c
@@ -12,21 +12,24 @@
 #include "utils/memutils.h"
 #include "catalog/namespace.h"
 
-PG_MODULE_MAGIC;
+#define DSA_TRANCHE_APP 1
 
-#define LWTRANCHE_APP 1
+PG_MODULE_MAGIC;
 
 typedef struct
 {
     dshash_table_handle table_handle;
     dsa_handle area_handle;
-} handlers_t;
+} shared_handlers;
 
 typedef struct
 {
     Oid key;
     TimestampTz timestamp;
-} tracker_data_t;
+} tracker_entity;
+
+static shared_handlers *handlers = NULL;
+static ExecutorStart_hook_type prev_ExecutorStart = NULL;
 
 static void tracker_init(void);
 static void tracker_shutdown(void);
@@ -36,13 +39,9 @@ static dshash_table *tracker_attach_hash_table(dsa_area *seg);
 static void tracker_detach_all(dshash_table *table, dsa_area *seg);
 static uint32 oid_key_hash(const void *key, size_t size, void *arg);
 
-static ExecutorStart_hook_type prev_ExecutorStart = NULL;
-
-static handlers_t *handlers = NULL;
-
 static const dshash_parameters dshash_params = {
     .key_size = sizeof(Oid),
-    .entry_size = sizeof(tracker_data_t),
+    .entry_size = sizeof(tracker_entity),
     .hash_function = oid_key_hash,
     .compare_function = dshash_memcmp,
 };
@@ -57,52 +56,6 @@ PG_FUNCTION_INFO_V1(get_last_timestamps);
 static uint32 oid_key_hash(const void *key, size_t size, void *arg)
 {
     return oid_hash(key, size);
-}
-
-static void tracker_init(void)
-{
-    bool found;
-    dsa_area *seg = NULL;
-    dshash_table *table = NULL;
-
-    handlers = (handlers_t *)ShmemInitStruct("table_tracker_handlers", sizeof(handlers_t), &found);
-
-    if (found)
-        return;
-
-    memset(handlers, 0, sizeof(handlers_t));
-
-    seg = dsa_create(LWTRANCHE_APP);
-    if (!seg)
-        ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("could not create dynamic shared area")));
-
-    table = dshash_create(seg, &dshash_params, NULL);
-    if (!table)
-    {
-        dsa_detach(seg);
-        ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("could not create hash table")));
-    }
-
-    handlers->area_handle = dsa_get_handle(seg);
-    handlers->table_handle = dshash_get_hash_table_handle(table);
-
-    dsa_pin(seg);
-    dsa_pin_mapping(seg);
-
-    tracker_detach_all(table, seg);
-}
-
-static void tracker_shutdown(void)
-{
-    if (handlers)
-    {
-        dsa_area *seg = tracker_attach_dsa();
-        if (seg)
-        {
-            dsa_unpin(seg);
-            dsa_detach(seg);
-        }
-    }
 }
 
 static bool tracker_ensure_initialized(void)
@@ -153,7 +106,7 @@ Datum is_table_tracked(PG_FUNCTION_ARGS)
     bool found;
 
     dshash_table *table = NULL;
-    tracker_data_t *entry;
+    tracker_entity *entry;
     dsa_area *seg = NULL;
 
     if (PG_ARGISNULL(0))
@@ -184,7 +137,7 @@ Datum enable_table_tracking(PG_FUNCTION_ARGS)
     bool found;
 
     dshash_table *table = NULL;
-    tracker_data_t *entry;
+    tracker_entity *entry;
     dsa_area *seg = NULL;
 
     if (PG_ARGISNULL(0))
@@ -250,7 +203,7 @@ Datum get_last_timestamp(PG_FUNCTION_ARGS)
 
     dsa_area *seg = NULL;
     dshash_table *table = NULL;
-    tracker_data_t *entry = NULL;
+    tracker_entity *entry = NULL;
 
     if (PG_ARGISNULL(0))
         PG_RETURN_NULL();
@@ -308,7 +261,7 @@ Datum get_last_timestamps(PG_FUNCTION_ARGS)
 
     for (int i = 0; i < num_tables; i++)
     {
-        tracker_data_t *entry = NULL;
+        tracker_entity *entry = NULL;
 
         if (nulls[i])
         {
@@ -348,7 +301,7 @@ Datum set_last_timestamp(PG_FUNCTION_ARGS)
 
     dsa_area *seg = NULL;
     dshash_table *table = NULL;
-    tracker_data_t *entry = NULL;
+    tracker_entity *entry = NULL;
 
     if (PG_ARGISNULL(0))
         PG_RETURN_BOOL(false);
@@ -398,7 +351,7 @@ static void track_executor_start(QueryDesc *queryDesc, int eflags)
             {
                 dsa_area *seg = NULL;
                 dshash_table *table = NULL;
-                tracker_data_t *entry;
+                tracker_entity *entry;
 
                 if (!tracker_ensure_initialized())
                     break;
@@ -425,6 +378,43 @@ static void track_executor_start(QueryDesc *queryDesc, int eflags)
         standard_ExecutorStart(queryDesc, eflags);
 }
 
+static void tracker_init(void)
+{
+    bool found;
+    dsa_area *seg = NULL;
+    dshash_table *table = NULL;
+
+    handlers = (shared_handlers *)ShmemInitStruct("table_tracker_handlers", sizeof(shared_handlers), &found);
+
+    if (found)
+    {
+        return;
+    }
+
+    memset(handlers, 0, sizeof(shared_handlers));
+
+    seg = dsa_create(DSA_TRANCHE_APP);
+    if (!seg)
+    {
+        ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("could not create dynamic shared area")));
+    }
+
+    table = dshash_create(seg, &dshash_params, NULL);
+    if (!table)
+    {
+        dsa_detach(seg);
+        ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("could not create hash table")));
+    }
+
+    handlers->area_handle = dsa_get_handle(seg);
+    handlers->table_handle = dshash_get_hash_table_handle(table);
+
+    dsa_pin(seg);
+    dsa_pin_mapping(seg);
+
+    tracker_detach_all(table, seg);
+}
+
 void _PG_init(void)
 {
     tracker_init();
@@ -435,11 +425,24 @@ void _PG_init(void)
     ereport(LOG, (errmsg("Table tracker extension initialized")));
 }
 
+static void tracker_shutdown(void)
+{
+    if (handlers)
+    {
+        dsa_area *seg = tracker_attach_dsa();
+        if (seg)
+        {
+            dsa_unpin(seg);
+            dsa_detach(seg);
+        }
+    }
+}
+
 void _PG_fini(void)
 {
-    ExecutorStart_hook = prev_ExecutorStart;
-
     tracker_shutdown();
+
+    ExecutorStart_hook = prev_ExecutorStart;
 
     ereport(LOG, (errmsg("Table tracker extension cleaned up")));
 }
